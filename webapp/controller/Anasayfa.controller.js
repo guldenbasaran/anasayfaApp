cd8119dbaab22556992db16f84385eddce48e028
Gulden Basaran <guldenbasaran61@gmail.com>
2020-09-02 15:48:28 +0000

    first commit

---
+++ webapp/controller/Anasayfa.controller.js
@@ -0,0 +1,18 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller",
+	"sap/ui/model/json/JSONModel"
+	
+],
+
+function (Controller, JSONModel) {
+	"use strict";
+
+	return Controller.extend("exp_anasayfa.controller.Anasayfa", {
+
+	
+		
+		
+
+	});
+
+});
